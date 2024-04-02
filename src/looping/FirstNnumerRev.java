c1b84ad2ba649085296169342a8e1e093f3aeaf5
ramesh <rameshtm99@gmail.com>
2024-04-02 16:15:46 -0400

    Print first n numbers in the reverse order

---
+++ src/looping/FirstNnumerRev.java
@@ -0,0 +1,11 @@
+package looping;
+
+public class FirstNnumerRev {
+    public static void main(String[] args) {
+        System.out.println("The first N natural numbers in Reverse");
+        for (int i=10;i>=1;i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
