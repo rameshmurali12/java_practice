5d472e619984350ef60746c02111f306e3fe7d72
ramesh <rameshtm99@gmail.com>
2024-04-02 22:57:31 -0400

    Check if the given number is even or odd

--- src/looping/EvenOddChecker.java
+++ src/looping/EvenOddChecker.java
@@ -7,13 +7,14 @@ public class EvenOddChecker {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Enter number");
         int number = myObj.nextInt();
-        // Checking if number is even or odd number
-        if (number%2==0){
+        if (number%2==0)
+        {
             // If remainder is zero then this number is even
             System.out.println("Entered number is Even");
-        }else {
-            // If remainder is not zero then this number is
-            // odd
+        }
+        else
+        {
+            // If remainder is not zero then this number is odd
             System.out.println("Entered number is Odd");
         }
     }
